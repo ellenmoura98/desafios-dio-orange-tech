b9279324ad406642f8f3b066e441eff38316b601
ellenmoura98 <ellenkdiogenes2@gmail.com>
2022-12-03 20:20:19 -0300

    adicionando uma nova janela para mais informações dos pokemons

--- primeiros-passos-javascript/pokedex/assets/js/main.js
+++ primeiros-passos-javascript/pokedex/assets/js/main.js
@@ -6,10 +6,12 @@ const limit = 10;
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
+  const pokemonId = "pokemonNumberId" + pokemon.number;
+  const pokemonNameId = "pokemonNameId" + pokemon.number;
   return `
       <li class="pokemon ${pokemon.type}">
-          <span class="number">#${pokemon.number}</span>
-          <span class="name">${pokemon.name}</span>
+          <span class="number" id=${pokemonId}>#${pokemon.number}</span>
+          <span class="name" id=${pokemonNameId}>${pokemon.name}</span>
           <div class="detail">
               <ol class="types">
                   ${pokemon.types
@@ -19,8 +21,51 @@ function convertPokemonToLi(pokemon) {
               <img src="${pokemon.photo}"
                    alt="${pokemon.name}">
           </div>
+          <div class="maisDetalhesPokemons">
+            <button id="informationButton" onclick="DetailPokemon(${
+              pokemon.number
+            })" type="button">
+              Details
+            </button>
+         </div>
       </li>
+
   `;
+}
+
+async function DetailPokemon(pokemonId) {
+  const pokemonNumberId = "pokemonNumberId" + pokemonId;
+  const numberPokemon = document.getElementById(pokemonNumberId).innerHTML;
+  const pokemonNameId = "pokemonNameId" + pokemonId;
+  const namePokemon = document.getElementById(pokemonNameId).innerHTML;
+
+  const result = await fetch(
+    `https://pokeapi.co/api/v2/characteristic/${pokemonId}`
+  )
+    .then((response) => response.json())
+    .then((descriptions) => descriptions);
+
+  const descriptions = result.descriptions;
+
+  let descriptionPokemon = "";
+
+  descriptions.forEach((description) => {
+    if (description.language.name === "en") {
+      descriptionPokemon = description.description;
+      return;
+    }
+  });
+
+  alert(
+    "Number: " +
+      numberPokemon +
+      "\n" +
+      "Name: " +
+      namePokemon +
+      "\n" +
+      "Description: " +
+      descriptionPokemon
+  );
 }
 
 function loadPokemonItens(offset, limit) {
--- primeiros-passos-javascript/pokedex/assets/js/poke-api.js
+++ primeiros-passos-javascript/pokedex/assets/js/poke-api.js
@@ -24,6 +24,8 @@ pokeApi.getPokemonDetail = (pokemon) => {
 
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+  /* const url_characteristic = `https://pokeapi.co/api/v2/characteristic?offset=${offset}&limit=${limit}`
+  const url_ability = `https://pokeapi.co/api/v2/ability?offset=${offset}&limit${limit}`   */
 
   return fetch(url)
     .then((response) => response.json())
@@ -31,6 +33,25 @@ pokeApi.getPokemons = (offset = 0, limit = 10) => {
     .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
     .then((detailRequests) => Promise.all(detailRequests))
     .then((pokemonsDetails) => pokemonsDetails);
+};
+
+pokeApi.pokemonDetails = async (id) => {
+  console.log(" id  ", id);
+  const result = await fetch(`https://pokeapi.co/api/v2/characteristic/${id}`)
+    .then((response) => response.json())
+    .then((descriptions) => descriptions);
+  const descriptions = result.descriptions;
+  let descriptionPokemon = "";
+
+  descriptions.forEach((description) => {
+    //console.log(description.description);
+    if (description.language.name === "en") {
+      descriptionPokemon = description.description;
+      return;
+    }
+    console.log(descriptionPokemon);
+    return descriptionPokemon;
+  });
 };
 
 /* Promise.all([
