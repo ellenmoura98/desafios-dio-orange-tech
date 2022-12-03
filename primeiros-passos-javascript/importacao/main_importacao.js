a173bdb3f4241c71596354c43ffc4cfb6f94fb6f
ellenmoura98 <ellenkdiogenes2@gmail.com>
2022-12-03 12:19:50 -0300

    projeto base do pokedex desenvolvido durante as aulas

---
+++ primeiros-passos-javascript/importacao/main_importacao.js
@@ -0,0 +1,29 @@
+/* Uma sala contem 5 alunos e para cada aluno foi sorteado um número de 1 - 100.
+Faça um programa que receba os 5 números sorteados para os alunos e mostre o maior número sorteado.
+
+Dados de entrada:
+5
+50
+10
+98
+23
+
+Saída:
+98 */
+
+const { gets, print } = require("./funcoes-auxiliares");
+
+const numerosSorteados = [];
+
+for (let i = 0; i < 5; i++) {
+  const numeroSorteados = gets();
+  numerosSorteados.push(numeroSorteados);
+}
+let maiorValor = 0;
+for (let i = 0; i < numerosSorteados.length; i++) {
+  const numeroSorteados = numerosSorteados[i];
+  if (numeroSorteados > maiorValor) {
+    maiorValor = numeroSorteados;
+  }
+  console.log(maiorValor);
+}
