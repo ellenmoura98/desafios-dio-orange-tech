1ff495a00ec10001545df278b7e118c11e91e3a5
ellenmoura98 <ellenkdiogenes2@gmail.com>
2022-12-15 22:23:16 -0300

    Criando Uma Wiki de Repositórios do GitHub Com React

---
+++ dio-plataforma/desafio-github-repositorio/src/components/ItemRepo/styles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+  width: 80%;
+
+  h3 {
+    font-size: 32px;
+    color: #fafafa;
+  }
+
+  p {
+    font-size: 16px;
+    color: #fafafa60;
+    margin-bottom: 20px;
+  }
+
+  a {
+    color: #ffff;
+    margin-top: 20px;
+    text-decoration: none;
+  }
+  a.remover {
+    color: #ff0000;
+    margin-top: 20px;
+    text-decoration: none;
+  }
+  hr {
+    color: #fafafa;
+    margin: 20px 0;
+  }
+`;
